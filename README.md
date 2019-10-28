02e3e4a7e5fdd637327eb3cdd2810c4692d43c93
Hubert Badocha <badocha.hubert@gmail.com>
2019-10-28 13:12:38 +0100

    select and run fux

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # Introduction
 
-This extension doesn't provide ocaml support in vscode, install one of available extensions for that. It's sole purpose is to format the ocaml code
+Extension to format ocaml code and run it in interactive terminal.
+
+This extension doesn't provide ocaml support in vscode, install one of available extensions for that.
 
 ## Features
 
 Formats your ocaml code.
+It formats your code predictably and folowing your favourite rules.
 You can add your own formatting guidlines by either choosing one of preset profiles or making your own.
 To make your own profile choose profile "own" and add formatting guidelines in .ocamlformat file in project's root as defined in ocamlformat docs. 
 
-## Requirements
+You can select fragment of code and run it in the ocaml interactive terminal in the vscode terminal by right clicking after selecting and choosing "Run code fragment in interactive console"
 
-Install ocamlformat (https://github.com/ocaml-ppx/ocamlformat).
+## Requirements
+You must have installed:
+* OCaml
+* ocamlformat (https://github.com/ocaml-ppx/ocamlformat).
 
 ## Extension Settings
 
--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,7 @@ const profiles = [
 ];
 
 function callOcamlFormatCommand(content: string, fileName: string, dir: string, profile: string): any {
-	const out = { formattedText: "", error: null };
+	const out = { text: "", error: null };
 	try {
 		const sanitizedContent = content.replace(/"/g, '\\"');
 		let profileString = '';
@@ -24,13 +24,27 @@ function callOcamlFormatCommand(content: string, fileName: string, dir: string, 
 				profileString = `--profile=${profile}`;
 			}
 		}
-		out.formattedText = execSync(`cd ${dir} && echo "${sanitizedContent}" | ocamlformat --name=${fileName} --enable-outside-detected-project ${profileString} -`).toString();
+		out.text = execSync(`cd ${dir} && echo "${sanitizedContent}" | ocamlformat --name=${fileName} --enable-outside-detected-project ${profileString} -`).toString();
 	}
 	catch (error) {
 		out.error = error.toString();
 		console.log(error);
 	}
 	return out;
+}
+
+
+function getTextForExecution(content: string, fileName: string = "ocaml-code") {
+	const ocamlFormatResponse = callOcamlFormatCommand(content, fileName, ".", "ocamlformat");
+	if (ocamlFormatResponse.error !== null) {
+		vscode.window.showErrorMessage(ocamlFormatResponse.error);
+		throw new Error("Formatting error exception");
+	}
+
+	const formattedText = ocamlFormatResponse.text + "\n";
+	const preparedText = formattedText.replace(/\n\n/g, ";;\n");
+	const sanitizedText = preparedText.replace(/\"/g, "\\\"");
+	return sanitizedText;
 }
 
 function getFileDir(path: string) {
@@ -63,7 +77,6 @@ function getFileName(path: string) {
 // your extension is activated the very first time the command is executed
 
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	vscode.languages.registerDocumentFormattingEditProvider({ scheme: 'file', language: 'ocaml' }, {
@@ -103,7 +116,31 @@ export function activate(context: vscode.ExtensionContext) {
 				return [vscode.TextEdit.insert(document.positionAt(0), "")];
 			}
 
-			return [vscode.TextEdit.replace(fullRange, ocamlFormatResponse.formattedText)];
+			return [vscode.TextEdit.replace(fullRange, ocamlFormatResponse.text)];
+		}
+	});
+
+	vscode.commands.registerCommand('ocaml-formatter.runCodeFragment', () => {
+		// The code you place here will be executed every time your command is executed
+		const editor = vscode.window.activeTextEditor;
+		if (editor !== undefined) {
+			// Display a message box to the user
+			const selection = editor.selection;
+			const selectedText = editor.document.getText(selection);
+			try {
+				let ocamlScript = getTextForExecution(selectedText);
+
+				const terminal = vscode.window.createTerminal("OCaml ");
+				terminal.show();
+				terminal.sendText(`(printf "${ocamlScript}"; rlwrap cat) | ocaml`);
+			}
+			catch (e) {
+				vscode.window.showErrorMessage(e);
+			}
+
+		}
+		else {
+			vscode.window.showErrorMessage("Editor is undefined");
 		}
 	});
 }
