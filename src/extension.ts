93e3ca414ef9a070091853702f3aed0a6fce0f6e
Hubert Badocha <badocha.hubert@gmail.com>
2022-06-24 16:02:53 +0200

    Improved code quality and redability

--- src/extension.ts
+++ src/extension.ts
@@ -4,85 +4,56 @@
 import * as vscode from "vscode";
 
 import { execSync } from "child_process";
+import { dirname, basename } from "path";
 
-const profiles = [
-  "conventional",
-  "janestreet",
-  "ocamlformat",
-  "own",
-];
-
-interface ocamlformatCallOutput {
-  text: string;
-  error: null | string;
-}
+const profiles = ["conventional", "janestreet", "ocamlformat", "own"];
 
 function callOcamlFormatCommand(
   content: string,
   fileName: string,
   dir: string,
   profile: string
-): ocamlformatCallOutput {
-  const out: ocamlformatCallOutput = { text: "", error: null };
-  try {
-    const settings = vscode.workspace.getConfiguration("ocaml-formatter");
-    const ocamlformatPath = <string>settings.get("ocamlformat-path");
+): string {
+  const cmd = buildCommand(fileName, profile);
+  console.log(cmd);
 
-    let command = "";
-    if (settings.get("eval-opam-env")) {
-      command += "eval $(opam env --readonly) && ";
-    }
-    command += `${ocamlformatPath} --name='${fileName}' --enable-outside-detected-project`;
-    if (profiles.includes(profile) && profile !== "own") {
-      command = command.concat(` --profile='${profile}'`);
-    }
-    command += " -";
-    console.log(command);
-    out.text = execSync(command, {
-      cwd: dir,
-      input: content,
-    }).toString();
-  } catch (error) {
-    out.error = error.toString();
-    console.error(error);
-  }
-  return out;
+  return execSync(cmd, {
+    cwd: dir,
+    input: content,
+  }).toString();
 }
 
-function formatOcamlFormatterError(error: string) {
-  const formatted = error.replace(
-    /(\n|\r|.)*--enable-outside-detected-project .*? -/g,
-    ""
+function buildCommand(fileName: string, profile: string): string {
+  const settings = vscode.workspace.getConfiguration("ocaml-formatter");
+  const ocamlformatPath = <string>settings.get("ocamlformat-path");
+
+  const command: string[] = [];
+  if (settings.get("eval-opam-env")) {
+    command.push("eval $(opam env --readonly)", "&&");
+  }
+  command.push(
+    `${ocamlformatPath}`,
+    `--name='${fileName}'`,
+    "--enable-outside-detected-project"
   );
-  return formatted;
+  if (profiles.includes(profile) && profile !== "own") {
+    command.push(`--profile='${profile}'`);
+  }
+  command.push("-");
+  return command.join(" ");
+}
+
+function formatOcamlFormatterError(error: Error) {
+  return error
+    .toString()
+    .replace(/(\n|\r|.)*--enable-outside-detected-project .*? -/g, "");
 }
 
 function getTextForExecution(
   content: string,
   fileName: string = "ocaml-code.ml"
 ) {
-  const useRegex = /#use ".*?"(?= *(?:;;)?)/g;
-  const uses = content.match(useRegex);
-  const usesArray = uses ? uses : [];
-  let output = "";
-  const splitByUseContent = content.split(useRegex);
-  for (const [i, part] of Object.entries(splitByUseContent)) {
-    const ocamlFormatResponse = callOcamlFormatCommand(
-      part,
-      fileName,
-      ".",
-      "ocamlformat"
-    );
-    if (ocamlFormatResponse.error !== null) {
-      vscode.window.showErrorMessage(ocamlFormatResponse.error);
-      throw new Error("Formatting error exception");
-    }
-    output += ocamlFormatResponse.text;
-    if (usesArray[parseInt(i)] !== undefined) {
-      output += usesArray[parseInt(i)] + "\n\n";
-    }
-  }
-  const formattedText = output + ";;";
+  const formattedText = getFormattedText(content, fileName);
   const noCommentsText = formattedText.replace(/\(\*(.|\n|\r)*?\*\)/g, "");
   const preparedText = noCommentsText.replace(/\nlet/g, ";;\nlet");
   const sanitizedText = preparedText.replace(/\"/g, '\\"');
@@ -91,115 +62,105 @@ function getTextForExecution(
   return escapePercentageSign;
 }
 
-interface getPathInterface {
-  error: null | string;
-  path: string;
-}
-
-function getFileDir(path: string): getPathInterface {
-  let out = { error: null, path: "" };
-  try {
-    out.path = execSync(`dirname '${path}'`).toString().split("\n")[0];
-  } catch (error) {
-    out.error = error.toString();
-    console.error(error);
+function getFormattedText(content: string, fileName: string): string {
+  const useRegex = /#use ".*?"(?= *(?:;;)?)/g;
+  const uses = content.match(useRegex) ?? [];
+  const output: string[] = [];
+  const splitByUseContent = content.split(useRegex);
+  for (const [i, part] of Object.entries(splitByUseContent)) {
+    try {
+      output.push(callOcamlFormatCommand(part, fileName, ".", "ocamlformat"));
+    } catch (error) {
+      throw new Error(formatOcamlFormatterError(<Error>error));
+    }
+    if (uses[parseInt(i)] !== undefined) {
+      output.push(uses[parseInt(i)], "\n\n");
+    }
   }
-  return out;
-}
-
-function getFileName(path: string): getPathInterface {
-  let out = { error: null, path: "" };
-  try {
-    out.path = execSync(`basename '${path}'`).toString().split("\n")[0];
-  } catch (error) {
-    out.error = error.toString();
-    console.error(error);
-  }
-  return out;
+  output.push(";;");
+  return output.join("");
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   vscode.languages.registerDocumentFormattingEditProvider(
     { scheme: "file", language: "ocaml" },
     {
-      provideDocumentFormattingEdits(
-        document: vscode.TextDocument
-      ): vscode.TextEdit[] {
-        const filePath = document.uri.fsPath;
-
-        const dirObj = getFileDir(filePath);
-
-        if (dirObj.error !== null) {
-          vscode.window.showErrorMessage(dirObj.error);
-          return [vscode.TextEdit.insert(document.positionAt(0), "")];
-        }
-        const dir = dirObj.path;
-
-        const fileNameObj = getFileName(filePath);
-
-        if (fileNameObj.error !== null) {
-          vscode.window.showErrorMessage(fileNameObj.error);
-          return [vscode.TextEdit.insert(document.positionAt(0), "")];
-        }
-        const fileName = fileNameObj.path;
-
-        const settings = vscode.workspace.getConfiguration("ocaml-formatter");
-        const profile = <string>settings.get("profile");
-
-        const fullText = document.getText();
-
-        const ocamlFormatResponse = callOcamlFormatCommand(
-          fullText,
-          fileName,
-          dir,
-          profile
-        );
-
-        const fullRange = new vscode.Range(
-          document.positionAt(0),
-          document.positionAt(fullText.length)
-        );
-
-        if (ocamlFormatResponse.error !== null) {
-          vscode.window.showErrorMessage(
-            formatOcamlFormatterError(ocamlFormatResponse.error)
-          );
-          return [vscode.TextEdit.insert(document.positionAt(0), "")];
-        }
-
-        return [vscode.TextEdit.replace(fullRange, ocamlFormatResponse.text)];
-      },
+      provideDocumentFormattingEdits,
     }
   );
 
-  vscode.commands.registerCommand("ocaml-formatter.runCodeFragment", () => {
-    // The code you place here will be executed every time your command is executed
-    const editor = vscode.window.activeTextEditor;
-    if (editor !== undefined) {
-      // Display a message box to the user
-      const selection = editor.selection;
-      const selectedText = editor.document.getText(selection);
-      try {
-        let ocamlScript = getTextForExecution(selectedText);
+  vscode.commands.registerCommand(
+    "ocaml-formatter.runCodeFragment",
+    runCodeFragment
+  );
+}
 
-        const terminal = vscode.window.createTerminal("OCaml");
-        terminal.show();
-        // terminal.sendText(`printf "${ocamlScript}"`);
+function provideDocumentFormattingEdits(
+  document: vscode.TextDocument
+): vscode.TextEdit[] {
+  const filePath = document.uri.fsPath;
 
-        terminal.sendText(`/usr/bin/env bash`);
-        terminal.sendText(`(printf '${ocamlScript}'; rlwrap cat) | ocaml`);
-      } catch (e) {
-        vscode.window.showErrorMessage(e);
-      }
+  const settings = vscode.workspace.getConfiguration("ocaml-formatter");
+  const profile = <string>settings.get("profile");
+
+  const fullText = document.getText();
+
+  try {
+    var ocamlFormatResponse = callOcamlFormatCommand(
+      fullText,
+      basename(filePath),
+      dirname(filePath),
+      profile
+    );
+  } catch (error) {
+    return errorHandler(document, formatOcamlFormatterError(<Error>error));
+  }
+
+  const fullRange = new vscode.Range(
+    document.positionAt(0),
+    document.positionAt(fullText.length)
+  );
+  return [vscode.TextEdit.replace(fullRange, ocamlFormatResponse)];
+}
+
+function errorHandler(
+  document: vscode.TextDocument,
+  error: string
+): vscode.TextEdit[] {
+  vscode.window.showErrorMessage(error);
+  return [vscode.TextEdit.insert(document.positionAt(0), "")];
+}
+
+function runCodeFragment() {
+  // The code you place here will be executed every time your command is executed
+  const editor = vscode.window.activeTextEditor;
+  if (editor === undefined) {
+    vscode.window.showErrorMessage("Editor is undefined");
+    return;
+  }
+
+  // Display a message box to the user
+  const selection = editor.selection;
+  const selectedText = editor.document.getText(selection);
+  try {
+    const ocamlScript = getTextForExecution(selectedText);
+
+    const terminal = vscode.window.createTerminal("OCaml");
+    terminal.show();
+
+    terminal.sendText(`/usr/bin/env bash`);
+    terminal.sendText(`(printf '${ocamlScript}'; rlwrap cat) | ocaml`);
+  } catch (e) {
+    if (e instanceof Error) {
+      vscode.window.showErrorMessage(e.toString());
     } else {
-      vscode.window.showErrorMessage("Editor is undefined");
+      vscode.window.showErrorMessage(String(e));
     }
-  });
+  }
 }
 
 // this method is called when your extension is deactivated
